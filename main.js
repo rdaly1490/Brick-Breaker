db2a20e3bc0bc3543e010e5d7a3ef6e30e0a06c8
Rob <rdaly1490@gmail.com>
2016-05-30 20:32:18 -0500

    need to refine collission detection a bit but pretty good

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var ballSpeedY = 5;
 
 var paddleWidth = 100;
 var paddleThickness = 10;
+var paddleDistFromEdge = 60;
 var paddleX = 400;
 
 window.onload = function() {
@@ -25,11 +26,16 @@ function updateMousePosition(e) {
 	var root = document.documentElement;
 
 	// for sanity sake: take mouse X coord and subtract out how far canvas is from left side
-	// and how far the user has scolled side to side.  Same for Y coord.
+	// and how far the user has scolled side to side.
 	var mouseX = e.clientX - rect.left - root.scrollLeft;
-	// var mouseY = e.clientY - rect.top - root.scrollTop;
 
 	paddleX = mouseX - paddleWidth/2;
+}
+
+function ballReset() {
+	ballSpeedX = 5;
+	ballX = canvas.width/2
+	ballY = canvas.height/2
 }
 
 function updateAll() {
@@ -41,24 +47,44 @@ function moveAll() {
 	ballX+= ballSpeedX;
 	ballY+= ballSpeedY;
 	
-	if (ballX < 0) {
+	if (ballX < 0) { // left
 		ballSpeedX *= -1;
 	}
-	if (ballX > canvas.width) {
+	if (ballX > canvas.width) { // right
 		ballSpeedX *= -1;
 	}	
-	if (ballY < 0) {
+	if (ballY < 0) { // top
 		ballSpeedY *= -1;
 	}
-	if (ballY > canvas.height) {
+	if (ballY > canvas.height) { // bottom
+		ballReset();
+	}
+
+	var paddleTopEdgeY = canvas.height - paddleDistFromEdge;
+	var paddleBottomEdgeY = paddleTopEdgeY + paddleThickness;
+	var paddleLeftEdgeX = paddleX;
+	var paddleRightEdgeX = paddleLeftEdgeX + paddleWidth;
+
+	if (ballY > paddleTopEdgeY && // below the top of paddle
+	 	ballY < paddleBottomEdgeY && // above bottom of paddle
+	 	ballX > paddleLeftEdgeX && // right of left-most edge of paddle
+	 	ballX < paddleRightEdgeX) { // left of right-most edge of paddle
+
 		ballSpeedY *= -1;
+
+		var centerOfPaddleX = paddleX + paddleWidth/2;
+		var ballDistFromPaddleCenterX = ballX - centerOfPaddleX;
+
+		// will only happen if collission detected by this if statement
+		// 0.35 helps scale down the speed a bit
+		ballSpeedX = ballDistFromPaddleCenterX * 0.35;
 	}
 }
 
 function drawAll() {
 	colorRect(0, 0, canvas.width, canvas.height, 'black');
 	colorCircle(ballX, ballY, 10, 'white');
-	colorRect(paddleX, canvas.height - paddleThickness, paddleWidth, paddleThickness);
+	colorRect(paddleX, canvas.height - paddleDistFromEdge, paddleWidth, paddleThickness);
 }
 
 function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
